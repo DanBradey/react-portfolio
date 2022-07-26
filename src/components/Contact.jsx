be29aa625a8cc49d04f1c11165a53dbb6c49f9e2
DanBradey <bradey795@gmail.com>
2022-07-27 08:10:44 +0930

    added more project files

---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+            <form method='post' action="https://getform.io/f/8c08da29-94eb-4b97-a919-9954344a6dbc" className="flex flex-col max-w-[600px] w-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-[#3c96bd] text-gray-300">Contact</p>
+                    <p className="text-gray-300 py-4">// Submit the form below or shoot me an email - bradey795@gmail.com</p>
+                </div>
+                <input className="bg-gray-300 p-2" type="text" placeholder="Name" name="name"/>
+                <input className="my-4 p-2 bg-gray-300" type="email" placeholder="Email" name="email"/>
+                <textarea name = "message" placeholder="Message" rows={10} className="bg-gray-300 p-2">
+
+                </textarea>
+                <button className="text-white border-2 hover:bg-[#3c96bd] hover:border-[#3c96bd] px-4 py-3 my-8 mx-auto flex items-center">Let's Collaborate</button>
+
+            </form>
+
+        </div>
+    )
+
+}
+
+export default Contact;
