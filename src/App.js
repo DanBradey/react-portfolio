2e667d3f858f40c859fec0d26e1187bf70cb7222
DanBradey <bradey795@gmail.com>
2022-09-18 23:08:41 +0930

    updated text throughout. added padding to contact form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Navbar/>
         <Home />
         <About />
-
         <Work/>
         <Contact/>
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
             <div className="flex flex-col justify-center items-center w-full h-full">
                 <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-                    <div className="sm:text-right pb-8 pl-4">
+                    <div className="sm:text-left pb-8 pl-4">
                         <p className="text-4xl font-bold inline border-b-4 border-[#3c96bd]">About</p>
                     </div>
                 </div>
@@ -13,18 +13,19 @@ const About = () => {
                 <div></div>
 
                 <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-                    <div className="sm:text-right text-4xl font-bold">
+                    <div className="sm:text-left text-4xl font-bold">
                         <p>
-                            Hi, I'm Dan. Nice to meet you. Please take a look around.
+                            Hi, I'm Dan. Please take a look around and check out my work.
                         </p>
                     </div>
 
                     <div>
                         <p>
-                            I am passionate about building excellent software that improves the lives of those around me.
-                            I specialise in creating software for clients ranging from individuals and small business
-                            all the way to large enterprise corporations. What would you do if you had a software
-                            expert available at your fingertips?
+                            I am passionate about building amazing websites that improve the lives of those around me.
+                            I specialise in web development for clients such as individuals and small businesses using
+                            the latest technologies to give your visitors the best possible experience. Reach out to me
+                            to find out what I can do for you.
+
                         </p>
                     </div>
                 </div>
