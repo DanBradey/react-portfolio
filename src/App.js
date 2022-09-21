099f8c38ebf079d600efb7e7181f6cffc168ff64
Dan Bradey <bradey795@gmail.com>
2022-09-21 12:12:31 +0930

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Work/>
         <Contact/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+
+        <footer
+            className="p-4 bg-[#0a192f] shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://danbradey.com/"
+                                                                                        className="hover:underline">Dan Bradey</a>. All Rights Reserved.
+    </span>
+
+        </footer>
+
+    )
+}
+    export default Footer;
