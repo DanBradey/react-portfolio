4237480c4a617d248659ee8e3451671a20805c75
Dan Bradey <bradey795@gmail.com>
2022-09-21 12:37:55 +0930

    minor tweaks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
 
         <footer
-            className="p-4 bg-[#0a192f] shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            className="p-4 bg-[#0a192f] md:flex md:items-center md:justify-between md:p-6">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://danbradey.com/"
                                                                                         className="hover:underline">Dan Bradey</a>. All Rights Reserved.
     </span>
