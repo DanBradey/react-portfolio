f784bec43ef7cecaecd5efd56b6a34c59171cb0a
Dan Bradey <bradey795@gmail.com>
2023-09-05 11:21:46 +0930

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -35,19 +35,16 @@ const Work = () => {
                         </div>
                     </div>
 
-                    <div style={{backgroundImage: `url(${realEstate}`}} className="shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div">
+                    <div style={{backgroundImage: `url(${smithautomotive_screenshot}`}} className="shadow-lg shadow-[#040c16] group container rounder-md flex justify-center items-center mx-auto content-div">
                         {/* Hover Effects */}
                         <div className="opacity-0 group-hover:opacity-100">
                             <span className="text-2xl font-bold text-white tracking-wider">
-                                React JS Application
+                                Car Servicing Business
 
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
+                                <a href="http://smithautomotive.danbradey.com">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
-                                </a>
-                                <a href="/">
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                                 </a>
                             </div>
                         </div>
